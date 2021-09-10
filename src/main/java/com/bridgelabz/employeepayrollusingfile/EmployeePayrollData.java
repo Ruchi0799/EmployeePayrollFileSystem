1301d3de0b1db0082acf44fd28237370e297dcee
Ruchi Mandve <ruchi.mandve@morningstar.com>
2021-09-10 13:28:03 +0530

    [ruchi]: implemented print employee data

--- src/main/java/com/bridgelabz/employeepayrollusingfile/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/employeepayrollusingfile/EmployeePayrollData.java
@@ -11,6 +11,30 @@ public class EmployeePayrollData {
         this.salary = salary;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
     @Override
     public String toString() {
         return "EmployeePayrollData{" +
